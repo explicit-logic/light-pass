1ba3cd53a6231c125cda8f7567afe5d8d8f77c0e
Bogdan Andrianov <bogd.an@proverstka.com>
2024-03-17 19:50:14 +0800

    PeerJS

--- src/components/organisms/Connect.tsx
+++ src/components/organisms/Connect.tsx
@@ -1,7 +1,60 @@
+// Modules
+import { useEffect, useState } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 
+// Helpers
+import { PeerConnection } from '../../helpers/peer';
+
+const url = 'https://explicit-logic.github.io/quiz-web-3/en';
+
 function Connect() {
-  const url = 'https://explicit-logic.github.io/quiz-web-1/en';
+  const [loading, setLoading] = useState(false);
+  const [receiverId, setReceiverId] = useState<string>();
+  const connectionUrl = `${url}?r=${receiverId}`;
+
+  useEffect(() => {
+    const connect = async () => {
+      setLoading(true);
+      const id = await PeerConnection.startPeerSession();
+      PeerConnection.onIncomingConnection((sender) => {
+        const senderId = sender.peer;
+        console.info("Student connected:", senderId);
+
+        void PeerConnection.sendConnection(senderId, {
+          type: 'welcome',
+          message: 'Teacher A',
+        });
+
+        PeerConnection.onConnectionDisconnected(senderId, () => {
+          console.info("Student disconnected:", senderId);
+        });
+        PeerConnection.onConnectionReceiveData(senderId, (data) => {
+          console.info("Data from student ", data, senderId);
+        });
+      });
+
+      setReceiverId(id);
+      setLoading(false);
+    };
+    connect();
+  }, []);
+
+  const sendBroadcast = async () => {
+    await PeerConnection.sendBroadcast({
+      type: 'broadcast',
+      message: 'Hi from teacher: ' + new Date().getTime(),
+    });
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <div>
+          Loading...
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -10,14 +63,15 @@ function Connect() {
       }}>
         <QRCodeSVG
           size={350}
-          value={url}
+          value={connectionUrl}
         />
       </div>
       <input
         disabled
         placeholder=""
-        value={url}
+        value={connectionUrl}
       />
+      <button type="button" onClick={sendBroadcast}>Broadcast</button>
     </div>
   );
 }
---
+++ src/helpers/peer.ts
@@ -0,0 +1,107 @@
+import Peer, { DataConnection } from 'peerjs';
+
+let peer: Peer | undefined;
+const connectionMap: Map<string, DataConnection> = new Map<string, DataConnection>();
+
+export interface Data {
+  type: string;
+  message: string;
+}
+
+export const PeerConnection = {
+  getPeer: () => peer,
+  startPeerSession: () => new Promise<string>((resolve, reject) => {
+    if (peer && peer.id) {
+      return resolve(peer.id);
+    }
+    try {
+      peer = new Peer();
+      peer.on('open', (id) => {
+        console.log('My ID: ' + id);
+        resolve(id);
+      }).on('error', (err) => {
+        console.log(err);
+        reject(err);
+      });
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  }),
+  closePeerSession: () => new Promise<void>((resolve, reject) => {
+    try {
+      if (peer) {
+        peer.destroy();
+        peer = undefined;
+      }
+      resolve();
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  }),
+  onIncomingConnection: (callback: (conn: DataConnection) => void) => {
+    peer?.on('connection', function (conn) {
+      console.log('Incoming connection: ' + conn.peer);
+      connectionMap.set(conn.peer, conn);
+      callback(conn);
+    });
+  },
+  onConnectionDisconnected: (id: string, callback: () => void) => {
+    if (!peer) {
+      throw new Error('Peer doesn\'t start yet');
+    }
+    if (!connectionMap.has(id)) {
+      throw new Error('Connection didn\'t exist');
+    }
+    const conn = connectionMap.get(id);
+    if (conn) {
+      conn.on('close', function () {
+        console.log('Connection closed: ' + id);
+        connectionMap.delete(id);
+        callback();
+      });
+    }
+  },
+  sendConnection: (id: string, data: Data): Promise<void> => new Promise((resolve, reject) => {
+    if (!connectionMap.has(id)) {
+      reject(new Error('Connection didn\'t exist'));
+    }
+    try {
+      const conn = connectionMap.get(id);
+      if (conn) {
+        void conn.send(data);
+      }
+    } catch (err) {
+      reject(err);
+    }
+    resolve();
+  }),
+  sendBroadcast: async (data: Data) => {
+    if (!connectionMap.size) {
+      console.warn('No connections!');
+      return;
+    }
+    for (const conn of connectionMap.values()) {
+      await conn.send(data);
+      console.info('Sent to: ', conn.peer);
+    }
+  },
+  onConnectionReceiveData: (id: string, callback: (f: Data) => void) => {
+    if (!peer) {
+      throw new Error('Peer doesn\'t start yet');
+    }
+    if (!connectionMap.has(id)) {
+      throw new Error('Connection didn\'t exist');
+    }
+    const conn = connectionMap.get(id);
+    if (conn) {
+      conn.on('data', function (receivedData) {
+        console.log('Receiving data from ' + id);
+        console.log(receivedData);
+        const data = receivedData as Data;
+        callback(data);
+      });
+    }
+  }
+};
