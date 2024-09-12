d21defb3c82a1693b44912446b0247bcc3585bd4
Bogdan Andrianov <bogd.an@proverstka.com>
2024-09-12 16:31:56 +0700

    Send messages to all

--- src/components/organisms/ConnectBar/components/MessageModal.tsx
+++ src/components/organisms/ConnectBar/components/MessageModal.tsx
@@ -1,5 +1,12 @@
 import { Dialog, DialogPanel, DialogTitle, Transition } from '@headlessui/react';
+import { useCallback, useState } from 'react';
 import { memo } from 'react';
+
+// Constants
+import { TYPES } from '@/constants/message';
+
+// Lib
+import { sendToAll } from '@/lib/peer/sendToAll';
 
 type Props = {
   close: () => void;
@@ -7,6 +14,22 @@ type Props = {
 };
 
 function MessageModal({ isOpen, close }: Props) {
+  const [sent, setSent] = useState(false);
+  const onSubmit = useCallback(async (e: React.SyntheticEvent) => {
+    const target = e.target as typeof e.target & {
+      message: { value: string };
+      reset: () => void;
+    };
+    const text = target.message.value;
+    await sendToAll(TYPES.message, { text });
+    setSent(true);
+
+    setTimeout(() => setSent(false), 1000);
+
+    e.preventDefault();
+    target.reset();
+  }, []);
+
   return (
     <Transition
       show={isOpen}
@@ -41,7 +64,7 @@ function MessageModal({ isOpen, close }: Props) {
                 <span className="sr-only">Close modal</span>
               </button>
             </DialogTitle>
-            <div className="p-4 space-y-6">
+            <form className="p-4 space-y-6" onSubmit={onSubmit}>
               <div className="col-span-2">
                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Message for all responders
@@ -53,13 +76,26 @@ function MessageModal({ isOpen, close }: Props) {
                   placeholder="Hey guys! Comm check..."
                 />
               </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Send
-              </button>
-            </div>
+              {sent ? (
+                <div className="inline-flex items-center justify-center gap-1 w-full text-white bg-green-500 font-medium rounded-lg text-sm px-5 py-2 text-center">
+                  <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path
+                      fill-rule="evenodd"
+                      d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  Message sent!
+                </div>
+              ) : (
+                <button
+                  type="submit"
+                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Send
+                </button>
+              )}
+            </form>
           </DialogPanel>
         </div>
       </Dialog>
